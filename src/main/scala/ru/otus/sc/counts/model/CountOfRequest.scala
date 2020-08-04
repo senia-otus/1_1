7d0b59222d1777e032ba58fa559a0ddbde5f83e3
nebiroz <kovalski2006@yandex.ru>
2020-08-04 13:42:39 +0300

    Недописал один док

--- src/main/scala/ru/otus/sc/counts/model/CountOfRequest.scala
+++ src/main/scala/ru/otus/sc/counts/model/CountOfRequest.scala
@@ -5,6 +5,6 @@ import ru.otus.sc.utils.model.RequireKey
 /**
   * Request for get the call count by key
   *
-  * @param key
+  * @param key name of call counter
   */
 case class CountOfRequest(key: String) extends RequireKey
