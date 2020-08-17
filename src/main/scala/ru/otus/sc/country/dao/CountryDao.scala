76cf2cbad18226ac066afb31faf8883255a6598d
Dmytro Selentsov <dselentsov@danavero.com>
2020-08-17 18:13:20 +0300

    removed storage from country dao

--- src/main/scala/ru/otus/sc/country/dao/CountryDao.scala
+++ src/main/scala/ru/otus/sc/country/dao/CountryDao.scala
@@ -3,21 +3,21 @@ package ru.otus.sc.country.dao
 import ru.otus.sc.country.model.Country
 
 trait CountryDao {
-  val storage: Map[String, Country]
-
   /**
    * Gets country by passed id
    * @param id: String
    * @return Option[Country]
    */
-  def getById(id: String): Option[Country] = storage.get(id)
+  def getById(id: String): Option[Country]
 }
 
 class CountryDaoImpl extends CountryDao{
-  override val storage: Map[String, Country] =
+  private[this] val storage: Map[String, Country] =
     Map(
       "RU"  -> Country("Russia", "Moscow"),
       "USA" -> Country("United States Of America", "Washington"),
       "UA"  -> Country("Ukraine", "Kyiv")
     )
+
+  override def getById(id: String): Option[Country] = storage.get(id)
 }
